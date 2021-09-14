dbc1ae772f521ae077e7d06af6b51eb0babfd6cb
jslee <jslee8909@naver.com>
2021-09-14 15:10:34 +0900

    [20210914] regist sample

---
+++ sample/vue-frontend/src/components/example.vue
@@ -0,0 +1,30 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+
+export default {
+   el : 'example',
+  data: () => ({
+    a:'',
+    b:''
+  }),
+  beforeCreate: function(){},
+  create: function(){},
+  beforeMount: function(){},
+  mounted: function(){},
+  beforeUpdate: function(){},
+  updated: function(){}, 
+  beforeDestroy: function(){},
+  destroyed: function(){},
+  methods: {
+      method1: function(){
+          
+      },
+      method2: function(){
+
+      }
+  }
+}
+</script>
